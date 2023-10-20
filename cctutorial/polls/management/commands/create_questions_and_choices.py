dad2ad6413d84471a99421453479d17e7e17d1a2
Kevin Yokley <kyokley2@gmail.com>
2023-10-19 19:02:01 -0500

    Display output

--- cctutorial/polls/management/commands/create_questions_and_choices.py
+++ cctutorial/polls/management/commands/create_questions_and_choices.py
@@ -4,6 +4,8 @@ from django.core.management.base import BaseCommand
 from polls.models import Question, Choice
 from faker import Faker
 
+QUESTION_COUNT = 4
+
 
 fake = Faker()
 
@@ -12,10 +14,11 @@ class Command(BaseCommand):
     help = 'Create 4 Questions and 3-6 Choices for each of those Question objects'
 
     def handle(self, *args, **kwargs):
-        question_count = 4
-        for i in range(question_count):
+        choice_count = 0
+
+        for i in range(QUESTION_COUNT):
             new_question = Question.objects.create(
-                question_text=f'{"".join(fake.words(nb=5, unique=True))}?',
+                question_text=f'{" ".join(fake.words(nb=5, unique=True))}?',
                 pub_date=timezone.now())
 
             for j in range(random.randint(3, 6)):
@@ -24,6 +27,17 @@ class Command(BaseCommand):
                     choice_text=fake.name(),
                     votes=random.randint(0, 10),
                 )
+                choice_count += 1
+
+        self.stdout.write(
+            f'Inserted {QUESTION_COUNT} Questions'
+        )
+        self.stdout.write(
+            f'Inserted {choice_count} Choices'
+        )
+        self.stdout.write(
+            self.style.SUCCESS('Done')
+        )
 
         # use the Django API models to create four new questions in the
         # database as well as 3-6 Choices/Answers for those questions.
